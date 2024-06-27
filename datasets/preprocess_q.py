9f0b847c9cc387005acc25253b6b66e0af6feb8a
Your Name <you@example.com>
2024-06-28 02:57:27 +0800

    重新创建git仓库

--- datasets/preprocess_q.py
+++ datasets/preprocess_q.py
@@ -13,7 +13,7 @@ from Quaternions import Quaternions
 from Pivots import Pivots
 from os import listdir, makedirs, system
 from os.path import exists
-
+from inference_bvh import process
 
 def get_skel(joints, parents):
     c_offsets = []
@@ -33,87 +33,6 @@ def softmax(x, **kw):
 
 def softmin(x, **kw):
     return -softmax(-x, **kw)
-
-
-def process(positions):
-    # F J 3
-    """Put on Floor"""
-    fid_l, fid_r = np.array([8, 9]), np.array([12, 13])
-    foot_heights = np.minimum(positions[:, fid_l, 1], positions[:, fid_r, 1]).min(
-        axis=1
-    )
-    floor_height = softmin(foot_heights, softness=0.5, axis=0)
-
-    positions[:, :, 1] -= floor_height
-
-    """ Add Reference Joint """
-    trajectory_filterwidth = 3
-    reference = positions[:, 0]
-    positions = np.concatenate([reference[:, np.newaxis], positions], axis=1)
-
-    """ Get Foot Contacts """
-    velfactor, heightfactor = np.array([0.15, 0.15]), np.array([9.0, 6.0])
-
-    feet_l_x = (positions[1:, fid_l, 0] - positions[:-1, fid_l, 0]) ** 2
-    feet_l_y = (positions[1:, fid_l, 1] - positions[:-1, fid_l, 1]) ** 2
-    feet_l_z = (positions[1:, fid_l, 2] - positions[:-1, fid_l, 2]) ** 2
-    feet_l_h = positions[:-1, fid_l, 1]
-    feet_l = (
-        ((feet_l_x + feet_l_y + feet_l_z) < velfactor) & (feet_l_h < heightfactor)
-    ).astype(np.float)
-
-    feet_r_x = (positions[1:, fid_r, 0] - positions[:-1, fid_r, 0]) ** 2
-    feet_r_y = (positions[1:, fid_r, 1] - positions[:-1, fid_r, 1]) ** 2
-    feet_r_z = (positions[1:, fid_r, 2] - positions[:-1, fid_r, 2]) ** 2
-    feet_r_h = positions[:-1, fid_r, 1]
-    feet_r = (
-        ((feet_r_x + feet_r_y + feet_r_z) < velfactor) & (feet_r_h < heightfactor)
-    ).astype(np.float)
-
-    """ Get Root Velocity """
-    velocity = (positions[1:, 0:1] - positions[:-1, 0:1]).copy()
-
-    """ Remove Translation """
-    positions[:, :, 0] = positions[:, :, 0] - positions[:, :1, 0]
-    positions[1:, 1:, 1] = positions[1:, 1:, 1] - (
-        positions[1:, :1, 1] - positions[:1, :1, 1]
-    )
-    positions[:, :, 2] = positions[:, :, 2] - positions[:, :1, 2]
-
-    """ Get Forward Direction """
-    # Original indices + 1 for added reference joint
-    sdr_l, sdr_r, hip_l, hip_r = 15, 19, 7, 11
-    across1 = positions[:, hip_l] - positions[:, hip_r]
-    across0 = positions[:, sdr_l] - positions[:, sdr_r]
-    across = across0 + across1
-    across = across / np.sqrt((across**2).sum(axis=-1))[..., np.newaxis]
-
-    direction_filterwidth = 20
-    forward = np.cross(across, np.array([[0, 1, 0]]))
-    forward = filters.gaussian_filter1d(
-        forward, direction_filterwidth, axis=0, mode="nearest"
-    )
-    forward = forward / np.sqrt((forward**2).sum(axis=-1))[..., np.newaxis]
-
-    """ Remove Y Rotation """
-    target = np.array([[0, 0, 1]]).repeat(len(forward), axis=0)
-    rotation = Quaternions.between(forward, target)[:, np.newaxis]
-    positions = rotation * positions
-
-    """ Get Root Rotation """
-    velocity = rotation[1:] * velocity
-    rvelocity = Pivots.from_quaternions(rotation[1:] * -rotation[:-1]).ps
-
-    """ Add Velocity, RVelocity, Foot Contacts to vector """
-    positions = positions[:-1]
-    positions = positions.reshape(len(positions), -1)
-    positions = np.concatenate([positions, velocity[:, :, 0]], axis=-1)
-    positions = np.concatenate([positions, velocity[:, :, 1]], axis=-1)
-    positions = np.concatenate([positions, velocity[:, :, 2]], axis=-1)
-    positions = np.concatenate([positions, rvelocity], axis=-1)
-    positions = np.concatenate([positions, feet_l, feet_r], axis=-1)
-
-    return positions, rotation
 
 
 """ This script generated the local/global motion decoupled data and
